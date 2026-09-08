2c61411837b167870f73b6483e86079ff6ab5d7e
Paulo Meton <paulo.meton@liveacademia.com.br>
2026-09-08 10:09:26 -0400

    corrige api

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+﻿const API_BASE_URL = window.location.hostname === 'localhost' || window.location.protocol === 'file:'
+  ? 'http://localhost:3000/api'
+  : '/api';
 
 function getToken() {
   return localStorage.getItem('token');
